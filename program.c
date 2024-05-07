4a40c419351dfca91c1b2622070b35033365fed7
Muhammad Usman <info@codelikechamp.com>
2024-05-07 13:02:17 +0500

    Update program.c

--- program.c
+++ program.c
@@ -5,7 +5,12 @@ int main() {
     int num2 = 20;
     int sum = num1 + num2;
 
+    printf("Address of num1: %p\n", (void *)&num1);
+    printf("Address of num2: %p\n", (void *)&num2);
+    printf("Address of sum: %p\n", (void *)&sum);
+
     printf("The sum of %d and %d is %d\n", num1, num2, sum);
 
     return 0;
 }
+
